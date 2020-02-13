aec6efb0b73678d23bab4c0f781b1682f35f2839
specter <mikexx1264@gmail.com>
2020-02-12 23:51:09 -0600

    Hotfix

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
-var image = document.getElementsByClassName('thumbnail');
-new simpleParallax(image);
+// var image = document.getElementsByClassName('thumbnail');
+// new simpleParallax(image);
 
 // var images = document.querySelectorAll('img');
 // new simpleParallax(images);
+
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image, {
+	maxTransition: 60
+});
