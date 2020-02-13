4d6621072b553f830718c01351a39b15c3caf190
specter <mikexx1264@gmail.com>
2020-02-12 22:15:22 -0600

    Parallax fixed - js rework - perfomance

--- scripts.js
+++ scripts.js
@@ -1,30 +1,5 @@
-var w = $(window).width();
-var h = $(window).heigth();
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image);
 
-if ($("section").hasClass("parallax1")) {
-  Createparallaxbg(".parallax1");
-}
-
-$(window).bind('scroll', function () {
-  if ($("section").hasClass("parallax1")) {
-    parallaxbg(".parallax1",".parallax1");
-  }
-});
-
-// Parallax Background Image Create 
-function Createparallaxbg(parallaxImage) {
-  var ParSecImg = $(parallaxImage).attr("data-image");
-  $(parallaxImage).attr("style", "background-image:url(" + ParSecImg + ");");
-}
-
-// Parallax Background Image ATTR ADD
-function parallaxImage(position, parallaxImage) {
-  var currentTop = $(window).scrollTop();
-  var ParSecPT = $(position).position().top;
-  if (currentTop > (ParSecPT - h)) {
-
-    $(parallaxImage).css({
-      "background-position": "center " + ((currentTop - ParSecPT) / 2 - (h/5)) + "px"
-    });
-  }
-}
+// var images = document.querySelectorAll('img');
+// new simpleParallax(images);
